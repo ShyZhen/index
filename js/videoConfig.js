577a2cb3f09a9377da403ee95c76762dd7a73e5d
huaixiu.zhen <huaixiu.zhen@gmail.com>
2017-11-10 09:58:09 +0800

    add video config

--- js/videoConfig.js
+++ js/videoConfig.js
@@ -2,8 +2,14 @@
  * Created by litblc on 2017/11/9.
  */
 
-$(document).ready(function(){
-    var currentItem = Math.floor(Math.random()*3);
-    var videoArr = ['static/Trio.mp4', 'static/Wildog.mp4', 'yellow.jpg'];
+$(document).ready(function() {
+    var currentItem = Math.floor(Math.random() * 5);
+    var videoArr = [
+        'static/Trio.mp4',
+        'static/Wildog.mp4',
+        'static/Workout.mp4',
+        'static/Riverside.mp4',
+        'static/Thirst.mp4'
+    ];
     $(".selector-div--video").attr('src', videoArr[currentItem])
 });
